ff2c3db84a8f0b130e329f9351e24720edc0077e
TakahiroMiyazawa <s16c1118uy@s.chibakoudai.jp>
2018-05-23 13:27:34 +0900

    2018/05/23_05

---
+++ deeplearning01/sin_cos_x.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#データの作成
+x=np.arange(0,6,0.1)#0から0.6まで0.1刻みで生成
+y1=np.sin(x)
+y2=np.cos(x)
+
+#グラフの描画
+plt.plot(x,y1,label="sin")
+plt.plot(x,y2,label="cos",linestyle="--")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin&cos')#タイトル
+plt.legend()#注釈を入れる
+plt.show()
